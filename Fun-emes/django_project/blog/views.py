4e83c671c3b2c14f528b040c80619ec9e4256ab6
garyxian <gary.xian26@gmail.com>
2020-03-22 18:45:41 -0400

    env removed

---
+++ Fun-emes/django_project/blog/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+posts = [
+    {
+        'author' : 'CoreyMS',
+        'title' :'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+def audio(request):
+    data = request.POST.get('record')
+    import speech_recognition as sr
+
+    # get audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak:")
+        audio = r.listen(source)
+
+    try:
+        output = " " + r.recognize_google(audio)
+    except sr.UnknownValueError:
+        output = "Could not understand audio"
+    except sr.RequestError as e:
+        output = "Could not request results; {0}".format(e)
+    data =output
+
+    return render(request,'blog/audio.html',{'data':data})
