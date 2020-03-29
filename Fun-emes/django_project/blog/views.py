663e0ed7bb10c99148a4704d7369d23be84a9b4d
garyxian <gary.xian26@gmail.com>
2020-03-29 16:24:56 -0400

    given sentence layout

--- Fun-emes/django_project/blog/views.py
+++ Fun-emes/django_project/blog/views.py
@@ -31,7 +31,36 @@ def own_sentence(request):
     return render(request, 'blog/own_sentence.html', {'title': 'Use Your Own Sentence'})
 
 def given_sentence(request):
-    return render(request, 'blog/given_sentence.html', {'title': 'Say This Sentence'})
+    score = "------"
+    data = request.POST.get('record')
+    import speech_recognition as sr
+
+    # get audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak:")
+        audio = r.listen(source)
+
+    try:
+        print(dir(audio))
+        print(audio.sample_rate)   
+        print(audio.sample_width) 
+        with open("microphone-results.wav", "wb") as f:
+            f.write(audio.get_wav_data())  
+        from scipy.io.wavfile import read
+        import numpy as np
+        a = read("microphone-results.wav") 
+        audio_arr = np.array(a[1],dtype=float)
+        print(audio_arr)
+
+        output = " " + r.recognize_google(audio)
+    except sr.UnknownValueError:
+        output = "Could not understand audio"
+    except sr.RequestError as e:
+        output = "Could not request results; {0}".format(e)
+    data =output
+
+    return render(request, 'blog/given_sentence.html', {'title': 'Say This Sentence', 'data':data, 'score':score})
 
 def audio(request):
     data = request.POST.get('record')
