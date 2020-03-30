a834d986348806a97740882370316e1a383c93b2
garyxian <gary.xian26@gmail.com>
2020-03-30 14:09:05 -0400

    generate sentence button, evaluate button

--- Fun-emes/django_project/blog/urls.py
+++ Fun-emes/django_project/blog/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
 		path('', views.launch, name='launch'),
         path('home/', views.home, name='home'),
-        path('audio/', views.audio, name='audio'),
         path('own_sentence/', views.own_sentence, name='own_sentence'),
         path('given_sentence/', views.given_sentence, name='given_sentence'),
 ]
--- Fun-emes/django_project/blog/views.py
+++ Fun-emes/django_project/blog/views.py
@@ -2,20 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
-posts = [
-    {
-        'author' : 'CoreyMS',
-        'title' :'Blog Post 1',
-        'content' : 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+import os
+import json
+score_dict = None
+score_dir = os.path.dirname((__file__))
+rel_path = "score.json"
+abs_file_path = os.path.join(score_dir, rel_path)
+with open(abs_file_path, 'r') as f:
+    score_dict = json.load(f)
+    score_dict = json.dumps(score_dict)
+
+sentence = "This is a sample sentence."
+sentence_list = sentence.split()
+sentence_json = json.dumps(sentence_list)
 
 def launch(request):
     return render(request, 'blog/launch.html')
@@ -31,7 +30,6 @@ def own_sentence(request):
     return render(request, 'blog/own_sentence.html', {'title': 'Use Your Own Sentence'})
 
 def given_sentence(request):
-    score = "------"
     data = request.POST.get('record')
     import speech_recognition as sr
 
@@ -42,9 +40,6 @@ def given_sentence(request):
         audio = r.listen(source)
 
     try:
-        print(dir(audio))
-        print(audio.sample_rate)   
-        print(audio.sample_width) 
         with open("microphone-results.wav", "wb") as f:
             f.write(audio.get_wav_data())  
         from scipy.io.wavfile import read
@@ -60,35 +55,35 @@ def given_sentence(request):
         output = "Could not request results; {0}".format(e)
     data =output
 
-    return render(request, 'blog/given_sentence.html', {'title': 'Say This Sentence', 'data':data, 'score':score})
+    return render(request, 'blog/given_sentence.html', {'title': 'Say This Sentence', 'data':data, 'score':score_dict, 'sentence': sentence_json})
 
-def audio(request):
-    data = request.POST.get('record')
-    import speech_recognition as sr
+# def audio(request):
+#     data = request.POST.get('record')
+#     import speech_recognition as sr
 
-    # get audio from the microphone
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Speak:")
-        audio = r.listen(source)
+#     # get audio from the microphone
+#     r = sr.Recognizer()
+#     with sr.Microphone() as source:
+#         print("Speak:")
+#         audio = r.listen(source)
 
-    try:
-        print(dir(audio))
-        print(audio.sample_rate)   
-        print(audio.sample_width) 
-        with open("microphone-results.wav", "wb") as f:
-            f.write(audio.get_wav_data())  
-        from scipy.io.wavfile import read
-        import numpy as np
-        a = read("microphone-results.wav") 
-        audio_arr = np.array(a[1],dtype=float)
-        print(audio_arr)
+#     try:
+#         print(dir(audio))
+#         print(audio.sample_rate)   
+#         print(audio.sample_width) 
+#         with open("microphone-results.wav", "wb") as f:
+#             f.write(audio.get_wav_data())  
+#         from scipy.io.wavfile import read
+#         import numpy as np
+#         a = read("microphone-results.wav") 
+#         audio_arr = np.array(a[1],dtype=float)
+#         print(audio_arr)
 
-        output = " " + r.recognize_google(audio)
-    except sr.UnknownValueError:
-        output = "Could not understand audio"
-    except sr.RequestError as e:
-        output = "Could not request results; {0}".format(e)
-    data =output
+#         output = " " + r.recognize_google(audio)
+#     except sr.UnknownValueError:
+#         output = "Could not understand audio"
+#     except sr.RequestError as e:
+#         output = "Could not request results; {0}".format(e)
+#     data =output
 
-    return render(request,'blog/audio.html',{'data':data})
+#     return render(request,'blog/audio.html',{'data':data})
