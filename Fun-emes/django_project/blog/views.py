65f28be756002921ec356eb1df39c609731cb6b3
garyxian <gary.xian26@gmail.com>
2020-03-30 22:43:27 -0400

    prepare to use Score.cal_score function

--- Fun-emes/django_project/blog/views.py
+++ Fun-emes/django_project/blog/views.py
@@ -15,15 +15,17 @@ sentence = "This is a sample sentence."
 sentence_list = sentence.split()
 sentence_json = json.dumps(sentence_list)
 
+
+# initialize Score object
+### from callBackend import Score
+### Score_test = Score(args.lexiconaddr, args.phonesaddr, args.sr, args.kaldi_workspace, args.utt_id)
+
+
 def launch(request):
     return render(request, 'blog/launch.html')
 
 def home(request):
     return render(request, 'blog/home.html')
-<<<<<<< HEAD
-=======
-
->>>>>>> 1b77ab56325b118de66249bb4e2bbde767140155
 
 def own_sentence(request):
     return render(request, 'blog/own_sentence.html', {'title': 'Use Your Own Sentence'})
@@ -36,17 +38,23 @@ def given_sentence(request):
     # get audio from the microphone
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
         print("Speak:")
         audio = r.listen(source)
 
     try:
         with open("microphone-results.wav", "wb") as f:
             f.write(audio.get_wav_data())
-        from scipy.io.wavfile import read
-        import numpy as np
-        a = read("microphone-results.wav")
-        audio_arr = np.array(a[1],dtype=float)
-        print(audio_arr)
+        ### text = "Hello how are you doing?"
+        ### udio = "/Users/garyxian/Desktop/JHU/AI_systems/CALL-proto/Fun-emes/django_project/microphone-results.wav"  
+        # from scipy.io.wavfile import read
+        # import numpy as np
+        # a = read("microphone-results.wav") 
+        # audio_arr = np.array(a[1],dtype=float)
+        # print(audio_arr)
+        ### score_output = Score_test.CalcScores(audio, text)
+        ### wav_id = PackZero(args.utt_id, 6)
+        ### output = json.dumps(score_output, open("/Users/garyxian/Desktop/JHU/AI_systems/CALL-proto/Fun-emes/django_project/score_wav%s.json"%wav_id, "w", encoding="utf-8"))
 
         output = " " + r.recognize_google(audio)
     except sr.UnknownValueError:
@@ -57,13 +65,12 @@ def given_sentence(request):
 
     from gtts import gTTS
     import os
-    tts = gTTS(text='Good morning', lang='en')
+    tts = gTTS(text='Hello how are you doing?', lang='en')
     tts.save("good.mp3")
     os.system("mpg321 good.mp3")
 
     return render(request, 'blog/given_sentence.html', {'title': 'Say This Sentence', 'data':data, 'score':score_dict, 'sentence': sentence_json})
 
-<<<<<<< HEAD
 
 from django.contrib.auth.models import User
 from django.http import JsonResponse
@@ -86,35 +93,3 @@ def validate_username(request):
 #     # score the wav file
 #     # send the resulting JSON file to frontend
 
-=======
-# def audio(request):
-#     data = request.POST.get('record')
-#     import speech_recognition as sr
-
-#     # get audio from the microphone
-#     r = sr.Recognizer()
-#     with sr.Microphone() as source:
-#         print("Speak:")
-#         audio = r.listen(source)
-
-#     try:
-#         print(dir(audio))
-#         print(audio.sample_rate)
-#         print(audio.sample_width)
-#         with open("microphone-results.wav", "wb") as f:
-#             f.write(audio.get_wav_data())
-#         from scipy.io.wavfile import read
-#         import numpy as np
-#         a = read("microphone-results.wav")
-#         audio_arr = np.array(a[1],dtype=float)
-#         print(audio_arr)
-
-#         output = " " + r.recognize_google(audio)
-#     except sr.UnknownValueError:
-#         output = "Could not understand audio"
-#     except sr.RequestError as e:
-#         output = "Could not request results; {0}".format(e)
-#     data =output
-
-#     return render(request,'blog/audio.html',{'data':data})
->>>>>>> 1b77ab56325b118de66249bb4e2bbde767140155
